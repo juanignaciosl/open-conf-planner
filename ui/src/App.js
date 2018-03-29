36ab85bec5a82225209721b325391935d92cdbdc
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2018-03-29 19:24:49 +0200

    Event registration form

--- ui/src/App.js
+++ ui/src/App.js
@@ -4,6 +4,8 @@ import {
 } from 'react-router-dom';
 import {translate} from 'react-i18next';
 
+import EventRegistrationForm from './EventRegistrationForm';
+
 import './App.css';
 
 class App extends Component {
@@ -17,6 +19,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <h1>{t('WhatsTheNameOfYourEvent?')}</h1>
+          <EventRegistrationForm/>
         </div>
       </Router>
     );
